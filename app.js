6afe25474baecbbffb8fa0bd8c623961a9bef37e
Eren Gündüz <70098566+mehmeterengunduz6@users.noreply.github.com>
2021-08-24 21:06:05 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,197 @@
+const menu = [
+    {
+        id: 1,
+        title: "Tteokbokki",
+        category: "Korea",
+        price: 10.99,
+        img:
+            "https://twoplaidaprons.com/wp-content/uploads/2020/09/tteokbokki-top-down-view-of-tteokbokki-in-a-bowl-500x500.jpg",
+        desc: `Spicy rice cakes, serving with fish cake.`,
+    },
+    {
+        id: 2,
+        title: "Chicken Ramen",
+        category: "Japan",
+        price: 7.99,
+        img:
+            "https://www.forkknifeswoon.com/wp-content/uploads/2014/10/simple-homemade-chicken-ramen-fork-knife-swoon-01.jpg",
+        desc: `Chicken noodle soup, serving with vegetables such as soy bean, green onion. In an optional you can ask for egg. `,
+    },
+    {
+        id: 3,
+        title: "Bibimbap",
+        category: "Korea",
+        price: 8.99,
+        img:
+            "https://dwellbymichelle.com/wp-content/uploads/2020/05/DWELL-bibimbap.jpg",
+        desc: `Boiling vegetables, serving with special hot sauce`,
+    },
+    {
+        id: 4,
+        title: "Dan Dan Mian",
+        category: "China",
+        price: 5.99,
+        img:
+            "https://www.savingdessert.com/wp-content/uploads/2019/02/Dan-Dan-Noodles-10.jpg",
+        desc: `Dan dan noodle, serving with green onion `,
+    },
+    {
+        id: 5,
+        title: "Yangzhou Fried Rice",
+        category: "China",
+        price: 12.99,
+        img:
+            "https://salu-salo.com/wp-content/uploads/2013/02/Yangzhou-Fried-Rice1.jpg",
+        desc: `Yangzhou style fried rice, serving with bean and pickles `,
+    },
+    {
+        id: 6,
+        title: "Onigiri",
+        category: "Japan",
+        price: 9.99,
+        img:
+            "https://www.manusmenu.com/wp-content/uploads/2017/08/Onigiri-3-1-of-1.jpg",
+        desc: `Rice Sandwich, serving with soy sauce`,
+    },
+    {
+        id: 7,
+        title: "Jajangmyeon",
+        category: "Korea",
+        price: 15.99,
+        img:
+            "https://www.curiouscuisiniere.com/wp-content/uploads/2020/04/Jajangmyeon-Korean-Noodles-in-Black-Bean-Sauce5.1200H-720x540.jpg",
+        desc: `Black bean sauce noodle, serving with green onion `,
+    },
+    {
+        id: 8,
+        title: "Ma Yi Shang Shu",
+        category: "China",
+        price: 12.99,
+        img:
+            "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/F688C2F6-86EC-46C4-B9C7-A6BA01DF7437/Derivates/32E3E72A-F786-406D-AF7F-B30980A9AC6C.jpg",
+        desc: `Hot pepper sauce noodle, serving with soy bean and onion`,
+    },
+    {
+        id: 9,
+        title: "Doroyaki",
+        category: "Japan",
+        price: 3.99,
+        img:
+            "https://www.justonecookbook.com/wp-content/uploads/2011/10/Dorayaki-New-500x400.jpg",
+        desc: `Red bean paste dessert, serving with honey.`,
+    },
+];
+
+const btnContainerDOM = document.querySelector('.btn-container');
+const secContainerDOM = document.querySelector('.section-center');
+
+const darkButton = `<button id='all-button' class="btn btn-outline-dark btn-item">All</button><button id='korea-button' class="btn btn-outline-dark btn-item">Korea</button><button id='japan-button' class="btn btn-outline-dark btn-item">Japan</button><button id='china-button' class="btn btn-outline-dark btn-item">China</button>`
+
+btnContainerDOM.innerHTML = `${darkButton}`;
+
+const allButton = document.getElementById('all-button');
+const koreaButton = document.getElementById('korea-button');
+const japanButton = document.getElementById('japan-button');
+const chinaButton = document.getElementById('china-button');
+
+allButton.addEventListener('click', showAll);
+koreaButton.addEventListener('click', showKorea);
+japanButton.addEventListener('click', showJapan);
+chinaButton.addEventListener('click', showChina);
+
+showAll();
+
+function showAll() {
+    secContainerDOM.innerHTML = '';
+    for (let i = 0; i < menu.length; i++) {
+        secContainerDOM.innerHTML += `<div class="d-flex flex-row mb-5 col-6">
+        <div style="width: 150px; height: 150px;"><img
+                src=${menu[i]['img']}
+                alt="" height="175" width="164"></div>
+        <div class="ms-5">
+            <div class="row">
+                <span>${menu[i]['title']}</span>
+                <span>${menu[i]['price']}</span>
+            </div>
+            <div style="width:50px; height: 1px; background-color:black;"></div>
+            ${menu[i]['desc']}
+        </div>
+    </div>
+    `
+    }
+}
+
+function showKorea() {
+    secContainerDOM.innerHTML = '';
+    for (let i = 0; i < menu.length; i++) {
+        if (menu[i]['category'] == 'Korea') {
+            secContainerDOM.innerHTML += `<div class="d-flex flex-row mb-5 col-6">
+        <div style="width: 150px; height: 150px;"><img
+                src=${menu[i]['img']}
+                alt="" height="175" width="164"></div>
+        <div class="ms-5">
+            <div class="row">
+                <span>${menu[i]['title']}</span>
+                <span>${menu[i]['price']}</span>
+            </div>
+            <div style="width:50px; height: 1px; background-color:black;"></div>
+            ${menu[i]['desc']}
+        </div>
+    </div>
+    `
+        } else {
+            console.log('gg');
+        }
+
+    }
+}
+
+function showJapan() {
+    secContainerDOM.innerHTML = '';
+    for (let i = 0; i < menu.length; i++) {
+        if (menu[i]['category'] == 'Japan') {
+            secContainerDOM.innerHTML += `<div class="d-flex flex-row mb-5 col-6">
+        <div style="width: 150px; height: 150px;"><img
+                src=${menu[i]['img']}
+                alt="" height="175" width="164"></div>
+        <div class="ms-5">
+            <div class="row">
+                <span>${menu[i]['title']}</span>
+                <span>${menu[i]['price']}</span>
+            </div>
+            <div style="width:50px; height: 1px; background-color:black;"></div>
+            ${menu[i]['desc']}
+        </div>
+    </div>
+    `
+        } else {
+            console.log('gg');
+        }
+
+    }
+}
+
+function showChina() {
+    secContainerDOM.innerHTML = '';
+    for (let i = 0; i < menu.length; i++) {
+        if (menu[i]['category'] == 'China') {
+            secContainerDOM.innerHTML += `<div class="d-flex flex-row mb-5 col-6">
+        <div style="width: 150px; height: 150px;"><img
+                src=${menu[i]['img']}
+                alt="" height="175" width="164"></div>
+        <div class="ms-5">
+            <div class="row">
+                <span>${menu[i]['title']}</span>
+                <span>${menu[i]['price']}</span>
+            </div>
+            <div style="width:50px; height: 1px; background-color:black;"></div>
+            ${menu[i]['desc']}
+        </div>
+    </div>
+    `
+        } else {
+            console.log('gg');
+        }
+
+    }
+}
